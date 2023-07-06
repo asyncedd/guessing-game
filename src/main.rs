cfbe2d3813474a6d675c2e4f6f721a6d1b71a051
asyncedd <isynqquwu@proton.me>
2023-07-07 07:09:40 +0900

    feat: Add better hints
    feat: Add better hints

--- src/main.rs
+++ src/main.rs
@@ -24,9 +24,12 @@ fn main() {
             println!("Let's play a game! I'm thinking of a number between {} and {}.", start, end);
             println!("You have 5 attempts to guess the number. Good luck!");
 
+            let mut biggest_guess = end;
+            let mut small_guess = start;
+
             while amount_of_guesses < 5 {
                 amount_of_guesses += 1;
-               let remaining_attempts = 5 - amount_of_guesses + 1;
+                let remaining_attempts = 5 - amount_of_guesses + 1;
                 println!("\nAttempt {}/5 | Remaining attempts: {}", amount_of_guesses, remaining_attempts);
                 let input = read_input("Enter your guess: ");
 
@@ -36,9 +39,15 @@ fn main() {
                             println!("Congratulations! You guessed the correct number: {}", random_number);
                             return;
                         } else if guess < random_number {
-                            println!("Too low! Try again.");
+                            if guess > small_guess {
+                                small_guess = guess;
+                            }
+                            println!("Too low! The number is between {} and {}.", small_guess, biggest_guess);
                         } else {
-                            println!("Too high! Try again.");
+                            if guess < biggest_guess {
+                                biggest_guess = guess;
+                            }
+                            println!("Too high! The number is between {} and {}.", small_guess, biggest_guess);
                         }
                     }
                     Err(_) => {
@@ -46,6 +55,7 @@ fn main() {
                     }
                 }
             }
+
 
             println!("\nGame over! You've reached the maximum number of guesses.");
             println!("The correct number was: {}", random_number);
