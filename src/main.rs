1b17ebaa5cfc3290402c4c8fb468dab2be072357
asyncedd <isynqquwu@proton.me>
2023-07-07 06:53:25 +0900

    feat: Improve the UX abit

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,12 @@ fn main() {
             let random_number: u32 = rng.gen_range(start..=end);
             let mut amount_of_guesses = 0;
 
+            println!("Let's play a game! I'm thinking of a number between {} and {}.", start, end);
+            println!("You have 5 attempts to guess the number. Good luck!");
+
             while amount_of_guesses < 5 {
                 amount_of_guesses += 1;
-                println!("Guess the number! ({}-{})", start, end);
+                println!("\nAttempt {}/5", amount_of_guesses);
                 let input = read_input("Enter your guess: ");
 
                 match input.parse::<u32>() {
@@ -32,9 +35,9 @@ fn main() {
                             println!("Congratulations! You guessed the correct number: {}", random_number);
                             return;
                         } else if guess < random_number {
-                            println!("Too low!");
+                            println!("Too low! Try again.");
                         } else {
-                            println!("Too high!");
+                            println!("Too high! Try again.");
                         }
                     }
                     Err(_) => {
@@ -43,7 +46,8 @@ fn main() {
                 }
             }
 
-            println!("You've reached the maximum number of guesses. The correct number was: {}", random_number);
+            println!("\nGame over! You've reached the maximum number of guesses.");
+            println!("The correct number was: {}", random_number);
         }
         _ => {
             println!("Invalid input. Please enter valid range values.");
